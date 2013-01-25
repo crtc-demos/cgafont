263e8840b0e8c886b9c7a5b4b74c586562bb6d0e
Simon Rawles <rawles@otso.rawles.net>
2013-01-25 23:23:50 +0000

    A very simple font viewer

---
+++ cgafont/viewer.pm
@@ -0,0 +1,22 @@
+package cgafont::viewer;
+our $VERSION = '1.00';
+use base 'Exporter';
+our @EXPORT = qw(draw);
+use warnings;
+use strict;
+
+# input: a font (a hash, each key of which is a character, corresponding to a value, which is an array of bitmapped integers for each line of the character)
+sub draw { 
+	if ( $#_ == -1 ) { return; } 
+	my %font = %{$_[0]};
+	foreach my $key ( keys %font ) { 
+		print "$key\n";
+		foreach my $line ( @{$font{$key}} ) { 
+			my $bits = substr(unpack("B32", pack("N", $line)), -8);
+			$bits =~ tr/01/.O/;
+			print "\t$bits\n";
+			}
+		}
+	}
+
+1;
--- makefont.pl
+++ makefont.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl
 
 use cgafont::patterns;
+use cgafont::viewer;
 use warnings;
 use strict;
 
 my @patterns = readpatterns("patterns/patterns.txt");
+
+my %test = ();
+my @achar = (124,130,130,130,130,130,124,0); $test{'0'} = \@achar;
+draw(\%test);
 
 # ...
